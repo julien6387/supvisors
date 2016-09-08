b0f97fb35a9158c9dcbdf132d94faee77a3f202f
Julien LE CLEACH <julien.6387@gmail.com>
2016-09-08 12:17:37 -0400

    refactoring

--- supervisors/xmlrpcclient.py
+++ supervisors/xmlrpcclient.py
@@ -21,100 +21,18 @@ import xmlrpclib
 
 from supervisor.xmlrpc import SupervisorTransport
 
-from supervisors.infosource import infoSource
-
 
 class XmlRpcClient(object):
-    def __init__(self, address):
-        self.transport = self._getRpcTransport(address)
+
+    def __init__(self, address, info_source):
+        self.info_source = info_source
+        self.transport = self.getRpcTransport(address)
         self.proxy = xmlrpclib.ServerProxy('http://{0}'.format(address), self.transport) if self.transport else None
 
-    def _getRpcTransport(self, address):
-        if infoSource.serverUrl:
-            serverUrl = infoSource.serverUrl.split(':')
+    def getRpcTransport(self, address):
+        if self.info_source.serverUrl:
+            serverUrl = self.info_source.serverUrl.split(':')
             if len(serverUrl) == 3:
                 serverUrl[1] = '//' + address
                 serverUrl = ':'.join(serverUrl)
-                return SupervisorTransport(infoSource.userName, infoSource.password, serverUrl)
-        return None
-
-
-# unit test expecting that a Supervisor Server is running at address below
-if __name__ == "__main__":
-    def createSupervisordInstance():
-        from supervisor.options import ServerOptions
-        from supervisor.supervisord import Supervisor
-        supervisord = Supervisor(ServerOptions())
-        supervisord.options.serverurl = 'http://localhost:60000'
-        supervisord.options.server_configs = [{'username': None, 'section': 'inet_http_server', 'password': None, 'port': 60000}]
-        return supervisord
-    # create logger
-    from supervisors.options import options
-    from supervisor.loggers import getLogger
-    options.logger = getLogger('/tmp/xmlrpcclient.log', 20, '%(asctime)s %(levelname)s %(message)s\n', False, 0, 0, True)
-    # assign supervisord info source
-    from supervisors.infosource import SupervisordSource
-    infoSource.source = SupervisordSource(createSupervisordInstance())
-    # test xml-rpc
-    client = XmlRpcClient('cliche01')
-    system = client.proxy.system
-    supervisor = client.proxy.supervisor
-    supervisors = client.proxy.supervisors
-    # calls
-    print system.listMethods()
-    print system.methodHelp('supervisor.getState')
-    print supervisor.getState()
-    print system.methodHelp('supervisor.getProcessInfo')
-    print supervisor.getProcessInfo('crash:segv')
-    print supervisor.getProcessInfo('firefox')
-    print supervisor.getProcessInfo('sample_test_2:sleep')
-    print system.methodHelp('supervisor.startProcess')
-    # print supervisor.startProcess('SupervisorsWeb')
-    print system.methodHelp('supervisor.restart')
-    #print supervisor.restart()
-    print system.methodHelp('supervisors.getAPIVersion')
-    print supervisors.getAPIVersion()
-    print system.methodHelp('supervisors.getSupervisorsState')
-    print supervisors.getSupervisorsState()
-    print system.methodHelp('supervisors.getMasterAddress')
-    print supervisors.getMasterAddress()
-    print system.methodHelp('supervisors.getAllRemoteInfo')
-    print supervisors.getAllRemoteInfo()
-    print system.methodHelp('supervisors.getRemoteInfo')
-    print supervisors.getRemoteInfo('cliche01')
-    print system.methodHelp('supervisors.getAllApplicationInfo')
-    print supervisors.getAllApplicationInfo()
-    print system.methodHelp('supervisors.getApplicationInfo')
-    print supervisors.getApplicationInfo('sample_test_2')
-    print system.methodHelp('supervisors.getProcessInfo')
-    print supervisors.getProcessInfo('crash:segv')
-    print supervisors.getProcessInfo('firefox')
-    print supervisors.getProcessInfo('sample_test_1:*')
-    print system.methodHelp('supervisors.getProcessRules')
-    print supervisors.getProcessRules('sample_test_2:*')
-    print system.methodHelp('supervisors.getConflicts')
-    print supervisors.getConflicts()
-    print system.methodHelp('supervisors.restartApplication')
-    from supervisors.types import DeploymentStrategies
-    print supervisors.restartApplication(DeploymentStrategies.CONFIG, 'sample_test_2')
-    print system.methodHelp('supervisors.stopApplication')
-    print supervisors.stopApplication('sample_test_2')
-    print system.methodHelp('supervisors.startApplication')
-    print supervisors.startApplication(DeploymentStrategies.CONFIG, 'sample_test_2')
-    print system.methodHelp('supervisors.restartProcess')
-    print supervisors.restartProcess(DeploymentStrategies.LESS_LOADED, 'sample_test_2:yeux_01')
-    print system.methodHelp('supervisors.stopProcess')
-    print supervisors.stopProcess('sample_test_2:yeux_01')
-    print system.methodHelp('supervisors.startProcess')
-    print supervisors.startProcess(DeploymentStrategies.LESS_LOADED, 'sample_test_2:yeux_01')
-    # different use of start/stop/restart
-    print 'supervisors.restartProcess - alt'
-    print supervisors.restartProcess(DeploymentStrategies.MOST_LOADED, 'sample_test_2:*')
-    print 'supervisors.stoprocess - alt'
-    print supervisors.stopProcess('sample_test_2:*')
-    print 'supervisors.startProcess - alt'
-    print supervisors.startProcess(DeploymentStrategies.MOST_LOADED, 'sample_test_2:*')
-    print system.methodHelp('supervisors.restart')
-    #print supervisors.restart()
-    print system.methodHelp('supervisors.shutdown')
-    #print supervisors.shutdown()
+                return SupervisorTransport(self.info_source.userName, self.info_source.password, serverUrl)
