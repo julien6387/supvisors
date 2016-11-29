a4341e6145a12d61d71f2a684a6c34459b5057a8
Julien LE CLEACH <julien.6387.dev@gmail.com>
2016-11-29 17:56:49 -0500

    new option to add a description in the hmi

--- supervisors/test/scripts/process_app.py
+++ supervisors/test/scripts/process_app.py
@@ -21,7 +21,7 @@ from Tkinter import *
 
 class ProcessAppTk(Tk):
 
-    def __init__(self, namespec):
+    def __init__(self, namespec, description):
         """ Initialization of the attributes. """
         Tk.__init__(self, None)
         self.title(u'Supervisors')
@@ -29,8 +29,10 @@ class ProcessAppTk(Tk):
         # create main form
         formWindow = PanedWindow(self, orient=VERTICAL)
         # display namespec into a PanedWindow
-        mainWindow = PanedWindow(formWindow, orient=HORIZONTAL)
+        mainWindow = PanedWindow(formWindow, orient=VERTICAL)
         mainWindow.add(Label(mainWindow, text=u'Supervisors namespec: {}'.format(namespec)))
+        if description:
+            mainWindow.add(Label(mainWindow, text=u'Supervisors description: {}'.format(description)))
         # add window to form and pack it
         formWindow.add(mainWindow)
         formWindow.pack()
@@ -49,11 +51,12 @@ if __name__ == "__main__":
     # get arguments
     import argparse
     parser = argparse.ArgumentParser(description='Start a dummy window named with a namespec.')
-    parser.add_argument('-n', '--namespec', required=True, help="the namespec of the program")
-    parser.add_argument('-x', '--xkill', type=int, metavar='SEC', help="kill the window after SEC seconds")
+    parser.add_argument('-n', '--namespec', required=True, help='the namespec of the program')
+    parser.add_argument('-x', '--xkill', type=int, metavar='SEC', help='kill the window after SEC seconds')
+    parser.add_argument('-d', '--description', metavar='DESC', help='an additional description')
     args = parser.parse_args()
     # create window
-    root = ProcessAppTk(args.namespec)
+    root = ProcessAppTk(args.namespec, args.description)
     # create auto-kill task
     if args.xkill is not None:
         root.after(args.xkill * 1000, root.quit)
