8faf315971b84d7d761f742e0f87b0cc969b43e1
Julien LE CLEACH <julien.6387.dev@gmail.com>
2016-11-29 17:58:04 -0500

    update script so as it can be started from test application

--- supervisors/test/scripts/supervisorctl_test.sh
+++ supervisors/test/scripts/supervisorctl_test.sh
@@ -10,6 +10,7 @@ function sendRequest() {
     echo
 }
 
+sleep 1
 
 # print global help
 sendRequest help
@@ -30,24 +31,24 @@ sendRequest rules
 sendRequest rules database:movie_server_02
 
 # command requests on processes
-sendRequest start_process CONFIG database:movie_server_01
-sendRequest restart_process LESS_LOADED database:movie_server_01
-sendRequest stop_process database:movie_server_01
+sendRequest start_process CONFIG my_movies:converter_01
+sendRequest restart_process LESS_LOADED my_movies:converter_01
+sendRequest stop_process my_movies:converter_01
 
-sendRequest start_args database:movie_server_01 -x 3
+sendRequest start_args my_movies:converter_02 -x 3 -d \'additional arguments\'
 sleep 3
 
-sendRequest start_process_args MOST_LOADED database:movie_server_01 -x 3
+sendRequest start_process_args MOST_LOADED my_movies:converter_02 -x 3 -d \'additional arguments\'
 sleep 3
 
 # command requests on applications
-sendRequest start_application LESS_LOADED database
 sendRequest restart_application MOST_LOADED database
 sendRequest stop_application database
+sendRequest start_application LESS_LOADED database
 
 # command requests on Supervisor
 sendRequest sreload
-sleep 30
 
-sendRequest sshutdown
-
+# WARN: cannot test anything after sreload because script is being killed
+# so a shell fork is done for this last test
+(sleep 60 ; sendRequest sshutdown) &
