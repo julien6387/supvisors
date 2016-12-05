6493820095ad1a8ee9f332447ae268730e263d3f
Julien LE CLEACH <julien.6387.dev@gmail.com>
2016-12-05 12:52:06 -0500

    update version display

--- supervisors/viewhandler.py
+++ supervisors/viewhandler.py
@@ -22,6 +22,7 @@ import urllib
 from supervisor.http import NOT_DONE_YET
 from supervisor.states import SupervisorStates, RUNNING_STATES, STOPPED_STATES
 
+from supervisors.rpcinterface import API_VERSION
 from supervisors.ttypes import AddressStates, SupervisorsStates
 from supervisors.utils import get_stats
 from supervisors.viewimage import process_image_contents
@@ -61,6 +62,8 @@ class ViewHandler(object):
             # blink main title in conciliation state
             if self.supervisors.fsm.state == SupervisorsStates.CONCILIATION and self.supervisors.context.conflicts():
                 root.findmeld('supervisors_mid').attrib['class'] = 'blink'
+            # set Supervisors version
+            root.findmeld('version_mid').content(API_VERSION)
             # write navigation menu and Address header
             self.write_navigation(root)
             self.write_header(root)
