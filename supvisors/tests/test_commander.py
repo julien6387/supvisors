cfd4feb43a3c2f0829c19a056f11b3cd8d7c7302
Julien Le Cléach <julien.6387.dev@gmail.com>
2017-02-17 13:45:18 +0100

    Update test_commander.py

--- supvisors/tests/test_commander.py
+++ supvisors/tests/test_commander.py
@@ -17,6 +17,7 @@
 # limitations under the License.
 # ======================================================================
 
+import sys
 import unittest
 
 from supvisors.tests.base import DummySupvisors
