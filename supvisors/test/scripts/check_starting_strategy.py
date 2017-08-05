628a269885e71d3f05198ad4f7a0b7ff8e3d2639
Julien LE CLEACH <julien.6387.dev@gmail.com>
2017-08-05 05:46:00 -0400

    starting strategy tests completed

--- supvisors/test/scripts/check_starting_strategy.py
+++ supvisors/test/scripts/check_starting_strategy.py
@@ -77,11 +77,10 @@ class StartingStrategyTest(RunningAddressesTest):
         self.loading = {info['address_name']: info['loading']
             for info in addresses_info
                 if info['statecode'] == AddressStates.RUNNING}
-        print self.loading
 
     def _start_converter(self, idx):
         """ Get the current loading status. """
-        self.local_supvisors.start_process(StartingStrategies.LESS_LOADED,
+        self.local_supvisors.start_process(self.strategy,
                                            'my_movies:converter_0%d' % idx)
         # wait for events
         event = self._get_next_process_event()
@@ -95,13 +94,56 @@ class StartingStrategyTest(RunningAddressesTest):
         # refresh the address loadings
         self._refresh_loading()
 
-    def _test_config(self):
-        """ Test the starting strategies iaw the rules defined. """
+    def test_config(self):
+        """ Test the CONFIG starting strategy.
+        Start converters and check they have been started on the first address
+        available defined in the program section of the rules file. """
+        print('### Testing CONFIG starting strategy')
+        self.strategy = StartingStrategies.CONFIG
+        # no address config for almost all converters (excepted 04 and 07)
+        # so applicable order is the one defined in the supvisors section,
+        # i.e. cliche01, cliche03, cliche02, cliche04 (not running)
+        # cliche01 is one at 15% initially
+        self._start_converter(0)
+        self.assertItemsEqual([40, 15, 5], self.loading.values())
+        # continue with cliche01
+        self._start_converter(1)
+        self.assertItemsEqual([65, 15, 5], self.loading.values())
+        # there is still place on cliche01
+        # try with converter_04 to check the alt config
+        self._start_converter(4)
+        self.assertItemsEqual([65, 15, 30], self.loading.values())
+        # there is still place on cliche01
+        self._start_converter(2)
+        self.assertItemsEqual([90, 15, 30], self.loading.values())
+        # cliche01 is full. cliche03 will be used now
+        self._start_converter(3)
+        self.assertItemsEqual([90, 15, 55], self.loading.values())
+        # there is still place on cliche03
+        # try with converter_07 to check the alt config
+        # cliche01 is full, so second address in config will be used (cliche02)
+        self._start_converter(7)
+        self.assertItemsEqual([90, 40, 55], self.loading.values())
+        # there is still place on cliche03
+        self._start_converter(5)
+        self.assertItemsEqual([90, 40, 80], self.loading.values())
+        # cliche01 is full. cliche02 will be used now
+        self._start_converter(6)
+        self.assertItemsEqual([90, 65, 80], self.loading.values())
+        self._start_converter(8)
+        self.assertItemsEqual([90, 90, 80], self.loading.values())
+        # last converter cannot be started: no resource left
+        with self.assertRaises(xmlrpclib.Fault) as exc:
+            self._start_converter(9)
+        self.assertEqual(Faults.ABNORMAL_TERMINATION, exc.exception.faultCode)
+        self.assertItemsEqual([90, 90, 80], self.loading.values())
 
     def test_less_loaded(self):
-        """ Test the LESS_LOADED starting strategy. """
-        # start a converter and check it has been started on the address
-        # having the lowest loading
+        """ Test the LESS_LOADED starting strategy.
+        Start converters and check they have been started on the address
+        having the lowest loading. """
+        print('### Testing LESS_LOADED starting strategy')
+        self.strategy = StartingStrategies.LESS_LOADED
         self._start_converter(0)
         self.assertItemsEqual([15, 15, 30], self.loading.values())
         self._start_converter(1)
@@ -123,12 +165,38 @@ class StartingStrategyTest(RunningAddressesTest):
         # last converter cannot be started: no resource left
         with self.assertRaises(xmlrpclib.Fault) as exc:
             self._start_converter(9)
-        print exc.exception.__dict__
         self.assertEqual(Faults.ABNORMAL_TERMINATION, exc.exception.faultCode)
         self.assertItemsEqual([90, 90, 80], self.loading.values())
 
-    def _test_most_loaded(self):
-        """ Test the starting strategies iaw the rules defined. """
+    def test_most_loaded(self):
+        """ Test the MOST_LOADED starting strategy.
+        Start converters and check they have been started on the address
+        having the highest loading. """
+        print('### Testing MOST_LOADED starting strategy')
+        self.strategy = StartingStrategies.MOST_LOADED
+        self._start_converter(0)
+        self.assertItemsEqual([40, 15, 5], self.loading.values())
+        self._start_converter(1)
+        self.assertItemsEqual([65, 15, 5], self.loading.values())
+        self._start_converter(2)
+        self.assertItemsEqual([90, 15, 5], self.loading.values())
+        self._start_converter(3)
+        self.assertItemsEqual([90, 40, 5], self.loading.values())
+        self._start_converter(4)
+        self.assertItemsEqual([90, 65, 5], self.loading.values())
+        self._start_converter(5)
+        self.assertItemsEqual([90, 90, 5], self.loading.values())
+        self._start_converter(6)
+        self.assertItemsEqual([90, 90, 30], self.loading.values())
+        self._start_converter(7)
+        self.assertItemsEqual([90, 90, 55], self.loading.values())
+        self._start_converter(8)
+        self.assertItemsEqual([90, 90, 80], self.loading.values())
+        # last converter cannot be started: no resource left
+        with self.assertRaises(xmlrpclib.Fault) as exc:
+            self._start_converter(9)
+        self.assertEqual(Faults.ABNORMAL_TERMINATION, exc.exception.faultCode)
+        self.assertItemsEqual([90, 90, 80], self.loading.values())
 
 
 def test_suite():
