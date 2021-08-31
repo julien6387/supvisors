39e15eaa7fdb127d2da0913937faa24b9c2e13b1
Julien LE CLEACH <julien.6387.dev@gmail.com>
2021-08-31 19:54:08 +0200

    add gathering use case

--- supvisors/test/use_cases/gathering/start.sh
+++ supvisors/test/use_cases/gathering/start.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
+export CUR_DATE=`date +'%y%m%d'`
+export CUR_TIME=`date +'%H%M%S'`
+
 test_dir=$(dirname "$(readlink -f "$0")")
 
 # start supervisor on all servers
 for i in cliche81 cliche82
 do
 	echo "start Supvisors on" $i
-	ssh $i "export DISPLAY=:0 ; cd $test_dir ; rm -f log/* ; supervisord -c etc/supervisord_server.conf"
+	ssh $i "export DISPLAY=:0 ; cd $test_dir ; rm -f log/* ; export CUR_DATE=$CUR_DATE ; export CUR_TIME=$CUR_TIME ; supervisord -c etc/supervisord_server.conf"
 done
 
 # start supervisor on all consoles
 for i in cliche83
 do
 	echo "start Supvisors on" $i
-	ssh $i "export DISPLAY=:0 ; cd $test_dir ; ./configure.sh ; rm -f log/* ; supervisord -c etc/supervisord_console.conf"
+	ssh $i "export DISPLAY=:0 ; cd $test_dir ; ./configure.sh ; rm -f log/* ; export CUR_DATE=$CUR_DATE ; export CUR_TIME=$CUR_TIME ; supervisord -c etc/supervisord_console.conf"
 done
 
 firefox http://localhost:61000 &
