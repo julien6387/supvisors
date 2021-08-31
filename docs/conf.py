f122da9bda757b21cb72d1c3420e2f0eda831be8
Julien LE CLEACH <julien.6387.dev@gmail.com>
2021-08-31 19:14:49 +0200

    update doc requirements

--- docs/conf.py
+++ docs/conf.py
@@ -23,21 +23,15 @@ from datetime import date
 
 # -- General configuration ------------------------------------------------
 
-# If your documentation needs a minimal Sphinx version, state it here.
-#
 # needs_sphinx = '1.0'
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
+# Sphinx extension module names
 extensions = ['sphinx.ext.autodoc', 'myst_parser']
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['.templates']
+templates_path = []
 
 # The suffix(es) of source filenames.
-# You can specify multiple suffix as a list of string:
-#
 source_suffix = ['.rst', '.md']
 
 # The master toctree document.
@@ -54,48 +48,18 @@ author = u'Julien Le Cléach'
 # |version| and |release|, also used in various other places throughout the
 # built documents.
 #
-# The short X.Y version.
+# The Supvisors version.
 parent = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(os.path.abspath(parent))
-
 version_txt = os.path.join(parent, 'supvisors/version.txt')
-supvisors_version = open(version_txt).read().split('=')[1].strip()
+version = release = open(version_txt).read().split('=')[1].strip()
 
-version = supvisors_version
-
-# The full version, including alpha/beta/rc tags.
-release = supvisors_version
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This patterns also effect to html_static_path and html_extra_path
 exclude_patterns = ['.build', 'Thumbs.db', '.DS_Store']
 
-# The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
-
 
 # -- Options for HTML output ----------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-# on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
-import os
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+html_theme = 'sphinx_rtd_theme'
 
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-#
-# html_theme_options = {}
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = []
