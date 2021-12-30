413f06689b8ed3bb0c9aa85bf5e45361c4d49cd5
Julien LE CLEACH <julien.6387.dev@gmail.com>
2021-12-30 18:14:08 +0100

    update start stop scripts for integration testing

--- supvisors/test/scripts/start_all.sh
+++ supvisors/test/scripts/start_all.sh
@@ -5,11 +5,17 @@ SCRIPTS_DIR=`dirname $(readlink -e $0)`
 TEST_DIR=`readlink -e $SCRIPTS_DIR/..`
 
 # start all instances
-for host in cliche81 cliche82 cliche83
+for host in ${@:-cliche81 cliche82 cliche83}
 do
     echo "start Supervisor on host" $host
-    ping -c 1 $host && ssh -X $host "cd $TEST_DIR ; export DISPLAY=:0 ; rm -rf log/* ; supervisord"
+    ping -c 1 $host 2>&1 >/dev/null && ssh -fX $host "cd $TEST_DIR ; rm -rf log/*
+      export DISPLAY=:0
+      export IDENTIFIER=$host
+      sed -i 's/identifier=.*$/identifier='\$IDENTIFIER'/' etc/supervisord.conf
+      supervisord"
 done
 
-# tail on supervisor & supvisors logs
-tail -f log/sup*visor..log
+cd $TEST_DIR
+pwd
+sleep 1
+tail -f -n +1 log/sup*
