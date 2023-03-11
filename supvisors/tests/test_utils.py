4be340c2a15f43ade4d2cbfcd07a2904045ca2b2
Julien LE CLEACH <julien.6387.dev@gmail.com>
2023-03-11 19:25:29 +0100

    fix SupervisorServerUrl testing

--- supvisors/tests/test_utils.py
+++ supvisors/tests/test_utils.py
@@ -66,33 +66,19 @@ def test_extract_process_info():
 def test_server_url():
     """ Test the SupervisorServerUrl class. """
     # test without authentication
-    env = {'SUPERVISOR_SERVER_URL': 'http://localhost:60000'}
+    env = {'SUPERVISOR_SERVER_URL': 'http://localhost:60000', 'DUMMY': 'dummy'}
     srv_url = SupervisorServerUrl(env)
-    assert srv_url.env is env
-    assert srv_url.parsed_url.geturl() == 'http://localhost:60000'
+    assert srv_url.env == env
     assert srv_url.authentication == ''
-    # without port
-    srv_url.update_url('cliche81')
-    assert env['SUPERVISOR_SERVER_URL'] == 'http://cliche81:60000'
-    assert srv_url.parsed_url.geturl() == 'http://cliche81:60000'
-    # with port
-    srv_url.update_url('cliche82', 61000)
-    assert env['SUPERVISOR_SERVER_URL'] == 'http://cliche82:61000'
-    assert srv_url.parsed_url.geturl() == 'http://cliche82:61000'
+    srv_url.update_url('10.0.0.1', 7777)
+    assert srv_url.env == {'SUPERVISOR_SERVER_URL': 'http://10.0.0.1:7777', 'DUMMY': 'dummy'}
     # test with authentication
-    env = {'SUPERVISOR_SERVER_URL': 'http://user:password@localhost:60000'}
+    env = {'SUPERVISOR_SERVER_URL': 'http://user:password@localhost:60000', 'DUMMY': 'dummy'}
     srv_url = SupervisorServerUrl(env)
-    assert srv_url.env is env
-    assert srv_url.parsed_url.geturl() == 'http://user:password@localhost:60000'
+    assert srv_url.env == env
     assert srv_url.authentication == 'user:password@'
-    # without port
-    srv_url.update_url('cliche81')
-    assert env['SUPERVISOR_SERVER_URL'] == 'http://user:password@cliche81:60000'
-    assert srv_url.parsed_url.geturl() == 'http://user:password@cliche81:60000'
-    # with port
-    srv_url.update_url('cliche82', 61000)
-    assert env['SUPERVISOR_SERVER_URL'] == 'http://user:password@cliche82:61000'
-    assert srv_url.parsed_url.geturl() == 'http://user:password@cliche82:61000'
+    srv_url.update_url('10.0.0.1', 7777)
+    assert srv_url.env == {'SUPERVISOR_SERVER_URL': 'http://user:password@10.0.0.1:7777', 'DUMMY': 'dummy'}
 
 
 def test_statistics_functions():
