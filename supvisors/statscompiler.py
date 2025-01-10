4c02569c3e9d581d51d04d62aadfbb5cb0d95774
Julien LE CLEACH <julien.6387.dev@gmail.com>
2025-01-10 19:12:57 +0100

    blank line

--- supvisors/statscompiler.py
+++ supvisors/statscompiler.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ======================================================================
+
 import time
 from typing import Dict, List, Optional, Tuple
 
