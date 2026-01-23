bacd6881fd2a622c8b9938becf16851a90b0c2f4
Julien LE CLEACH <julien.6387.dev@gmail.com>
2026-01-23 18:30:02 +0100

    test Python 3.14

--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ CLASSIFIERS = [
     "Programming Language :: Python :: 3.11",
     "Programming Language :: Python :: 3.12",
     "Programming Language :: Python :: 3.13",
+    "Programming Language :: Python :: 3.14",
     "Topic :: System :: Boot",
     "Topic :: System :: Monitoring",
     "Topic :: System :: Software Distribution"
